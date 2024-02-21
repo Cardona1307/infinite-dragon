4e3e91def417a702ae2439b62ef0b952daf1d0dc
Cardona1307 <jc.cardonav1307@gmail.com>
2024-02-20 21:16:52 -0500

    menu terminado

---
+++ Assets/scripts/Menú/MenúInicial.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenúInicial : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Salir del juego ");
+        Application.Quit();
+    }
+
+    public void Opciones()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void SalirMenú()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void Volver()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
